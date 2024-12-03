8bb0ab2322155f896b521821e3735b3e4e7b72cd
fayed <fayed_rana@outlook.com>
2024-12-03 16:48:48 +0100

    variables and assets added

--- main.py
+++ main.py
@@ -24,14 +24,14 @@ fps = 60
 white_pieces = ["rook", "knight", "bishop", "queen", "king", "bishop", "knight", "rook", 
                 "pawn", "pawn", "pawn", "pawn", "pawn", "pawn", "pawn", "pawn", ]
 #given location for each piece based on pygame grid, NOT chessgrid
-white_locations = [(0,0)(1,0),(2,0),(3,0),(4,0),(5,0),(6,0)(7,0),
-                  (0,1)(1,1),(2,1),(3,1),(4,1),(5,1),(6,1)(7,1)]
+white_locations = [(0,0),(1,0),(2,0),(3,0),(4,0),(5,0),(6,0),(7,0),
+                  (0,1),(1,1),(2,1),(3,1),(4,1),(5,1),(6,1),(7,1)]
 
 black_pieces = ["rook", "knight", "bishop", "queen", "king", "bishop", "knight", "rook", 
                 "pawn", "pawn", "pawn", "pawn", "pawn", "pawn", "pawn", "pawn", ]
 #same goes for the black pieces location
-black_locations = [(0,7)(1,7),(2,7),(3,7),(4,7),(5,7),(6,7)(7,7),
-                  (0,6)(1,6),(2,6),(3,6),(4,6),(5,6),(6,6)(7,6)]
+black_locations = [(0,7),(1,7),(2,7),(3,7),(4,7),(5,7),(6,7),(7,7),
+                  (0,6),(1,6),(2,6),(3,6),(4,6),(5,6),(6,6),(7,6)]
 #we will append to these lists when pieces get captured
 captured_pieces_white = []
 captured_pieces_black = []
